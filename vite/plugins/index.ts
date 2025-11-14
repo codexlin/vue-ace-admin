9f562832f1efbe10bd3c76abeabe04b9ebd370e5
CodexLin <codexlin@gmail.com>
2025-11-14 09:58:24 +0800

    fix(vite): 为 VITE_BUILD_COMPRESS 添加默认值，避免 Docker 构建时未定义错误

--- vite/plugins/index.ts
+++ vite/plugins/index.ts
@@ -7,7 +7,7 @@ import setupSvgIcon from './svg-icon'
 
 // 插件配置
 export default function (viteEnv: Record<string, string>, isBuild: boolean) {
-  const { VITE_BUILD_COMPRESS } = viteEnv
+  const { VITE_BUILD_COMPRESS = '' } = viteEnv
   const plugins: PluginOption[] = [
     ...setupCommonPlugins(),
     setupAutoImport(),
@@ -15,7 +15,7 @@ export default function (viteEnv: Record<string, string>, isBuild: boolean) {
     setupIcons(),
     setupSvgIcon(isBuild)
   ]
-  if (isBuild) {
+  if (isBuild && VITE_BUILD_COMPRESS) {
     const compressList = VITE_BUILD_COMPRESS.split(',')
     // if (compressList.includes('gzip') || compressList.includes('brotli')) plugins.push(...setupCompression(viteEnv))
   }
